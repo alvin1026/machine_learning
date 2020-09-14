b7c387b124f8771a5efc134be66d3e70eced7fac
alvin wang <alvin_1026@hotmail.com>
2020-09-13 21:25:33 -0400

    apply cross-validation

--- cross_validation.py
+++ cross_validation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import numpy as np
 
 
@@ -27,18 +27,29 @@ class LogisticModel:
         print("  recall: {0:.5f}".format(recall_score(self.y_test, self.y_pred)))
         print("  f1 score: {0:.5f}".format(f1_score(self.y_test, self.y_pred)))
 
+    def get_score(self):
+        return self.model.score(self.X_test, self.y_test)
+
 
 def main():
     df = pd.read_csv('./titanic.csv')
     df['male'] = df['Sex'] == 'male'
     X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
     y = df['Survived'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    m = LogisticModel(X_train, X_test, y_train, y_test)
-    m.build_model()
-    m.evaluate()
+    # Loop over all the folds
+    scores = []
+    kf = KFold(n_splits=5, shuffle=True)
 
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        m = LogisticModel(X_train, X_test, y_train, y_test)
+        m.build_model()
+        scores.append(m.get_score())
+    print(scores)
+    print(np.mean(scores))
 
 if __name__ == "__main__":
     main()
